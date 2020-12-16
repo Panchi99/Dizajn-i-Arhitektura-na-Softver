144010d6bdf653dc6849c65dfec9d28ae7246021
Panchi99 <panci_kostov@hotmail.com>
2020-12-17 00:31:00 +0100

    ContactMailSender
    Added functionality to send mail from contact page to devellopers desiganted mail.

--- Domashna 3/Dizajn i Arhitektura na Softver/Controllers/HomeController.cs
+++ Domashna 3/Dizajn i Arhitektura na Softver/Controllers/HomeController.cs
@@ -8,6 +8,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Dizajn_i_Arhitektura_na_Softver.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
 namespace Dizajn_i_Arhitektura_na_Softver.Controllers
 {
 
@@ -24,6 +29,74 @@ namespace Dizajn_i_Arhitektura_na_Softver.Controllers
 
             return View();
         }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(SendMail model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("petrolheadsbussiness@gmail.com"));  // replace with valid value 
+                message.From = new MailAddress(model.FromEmail);  // replace with valid value
+                message.Subject = model.Subject;
+
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                if (model.FromEmail.Contains("@hotmail") || model.FromEmail.Contains("@outlook") || model.FromEmail.Contains("@msn")
+                    || model.FromEmail.Contains("@live") || model.FromEmail.Contains("@msn"))
+                {
+
+                    using (var smtp = new SmtpClient())
+                    {
+                        smtp.UseDefaultCredentials = false;
+                        var credential = new NetworkCredential
+                        {
+                            UserName = model.FromEmail,  // replace with valid value
+                            Password = model.Password  // replace with valid value
+                        };
+                        smtp.Credentials = credential;
+                        smtp.Host = "smtp-mail.outlook.com";
+                        smtp.Port = 587;
+                        smtp.EnableSsl = true;
+                        await smtp.SendMailAsync(message);
+                        return RedirectToAction("Sent");
+                    }
+                }
+                else if (model.FromEmail.Contains("@gmail"))
+                {
+                    using (var smtp = new SmtpClient())
+                    {
+                        smtp.UseDefaultCredentials = false;
+                        var credential = new NetworkCredential
+                        {
+                            UserName = model.FromEmail,  // replace with valid value
+                            Password = model.Password  // replace with valid value
+                        };
+                        smtp.Credentials = credential;
+                        smtp.Host = "smtp.gmail.com";
+                        smtp.Port = 587;
+                        smtp.EnableSsl = true;
+                        await smtp.SendMailAsync(message);
+                        return RedirectToAction("Sent");
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Sent()
+        {
+            return View();
+        }
+
+
+
+
+
 
         public ActionResult Contact()
         {
---
+++ Domashna 3/Dizajn i Arhitektura na Softver/Models/SendMail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Dizajn_i_Arhitektura_na_Softver.Models
+{
+    public class SendMail
+    {
+        [Required, Display(Name = "Your name")]
+        public string FromName { get; set; }
+        [Required, Display(Name = "Your email"), EmailAddress]
+        public string FromEmail { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+        [Required, Display(Name ="Your Password"), DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
