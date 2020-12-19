a36b948bc1ab266585873ce88d5248adb1cdeb0e
DavidJandrijeski <74428189+DavidJandrijeski@users.noreply.github.com>
2020-12-19 22:16:33 +0100

    Server MapPath to BenziskiPumpi.csv

--- Domashna 3/Dizajn i Arhitektura na Softver/Models/Csv.cs
+++ Domashna 3/Dizajn i Arhitektura na Softver/Models/Csv.cs
@@ -51,7 +51,9 @@ namespace Dizajn_i_Arhitektura_na_Softver.Models
         }
         public void csvFile()
         {
-            using (var reader = new StreamReader("C:/Users/user/Documents/GitHub/Dizajn-i-Arhitektura-na-Softver/Domashna 3/Dizajn i Arhitektura na Softver/BenziskiPumpi.csv"))
+            string path = HttpContext.Current.Server.MapPath(@"../BenziskiPumpi.csv");
+            using (var reader = new StreamReader(path, true))
+               
             {
                 while (!reader.EndOfStream)
                 {
