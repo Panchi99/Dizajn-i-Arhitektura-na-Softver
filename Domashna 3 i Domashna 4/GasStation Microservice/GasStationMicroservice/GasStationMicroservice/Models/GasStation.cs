da8e4eafc66f3a5475d448abab70b10dec1f8d28
DavidJandrijeski <74428189+DavidJandrijeski@users.noreply.github.com>
2021-01-31 23:42:15 +0100

    MailMicroservice

---
+++ Domashna 3 i Domashna 4/GasStation Microservice/GasStationMicroservice/GasStationMicroservice/Models/GasStation.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GasStationMicroservice.Models
+{
+    public class GasStation
+    {
+        private List<String> longitude { get; set; }
+        private List<String> latitude { get; set; }
+
+        private List<String> ID { get; set; }
+
+        private List<String> Name { get; set; }
+
+        private List<String> openingHours { get; set; }
+
+        private List<String> cashPayment { get; set; }
+
+        private List<String> masterCardPayment { get; set; }
+
+        private List<String> visaPayment { get; set; }
+
+        private List<String> fuel95 { get; set; }
+
+        private List<String> fuel98 { get; set; }
+
+        private List<String> fuelDiesel { get; set; }
+
+        private List<String> fuelLPG { get; set; }
+
+        private List<String> AvgRating { get; set; }
+        private List<String> CountRatings { get; set; }
+        public int IDSelected { get; set; }
+        public List<SelectListItem> selectLists { get; set; }
+        public IEnumerable<SelectListItem> list { get; set; }
+    }
+}
---
+++ Domashna 3 i Domashna 4/Mail Microservice/MailMicroservice/MailMicroservice/Models/Mail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailMicroservice.Models
+{
+    public class Mail
+    {
+        [Required, Display(Name = "Your name")]
+        public string FromName { get; set; }
+        [Required, Display(Name = "Your email"), EmailAddress]
+        public string FromEmail { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+        [Required, Display(Name = "Your Password"), DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
